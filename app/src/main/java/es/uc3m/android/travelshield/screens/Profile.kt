24ad6fe26a9a7ce829b21bd0a11ab1d8646f33d1
mariamagro <mamgmadrid@gmail.com>
2025-03-02 20:37:01 +0100

    login en profile

--- app/src/main/java/es/uc3m/android/travelshield/screens/Profile.kt
+++ app/src/main/java/es/uc3m/android/travelshield/screens/Profile.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+
 @Composable
 fun ProfileScreen(navController: NavController) {
     var email by remember { mutableStateOf("") }

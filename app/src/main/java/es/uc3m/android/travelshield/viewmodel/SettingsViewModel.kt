8c7e9e4fc575d55d4f1569a9b7b04ffd26a1a740
miguel <100473125@alumnos.uc3m.es>
2025-04-23 01:43:21 +0200

    Mejoras Varias (mirar checklist)

---
+++ app/src/main/java/es/uc3m/android/travelshield/viewmodel/SettingsViewModel.kt
@@ -0,0 +1,29 @@
+package es.uc3m.android.travelshield.viewmodel
+
+import android.app.Application
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+
+val Context.dataStore by preferencesDataStore(name = "settings")
+
+class SettingsViewModel(application: Application) : AndroidViewModel(application) {
+    private val darkModeKey = booleanPreferencesKey("dark_mode_enabled")
+
+    val isDarkMode = application.dataStore.data
+        .map { preferences -> preferences[darkModeKey] ?: false }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), false)
+
+    fun setDarkMode(enabled: Boolean) {
+        viewModelScope.launch {
+            getApplication<Application>().dataStore.edit { settings ->
+                settings[darkModeKey] = enabled
+            }
+        }
+    }
+}
